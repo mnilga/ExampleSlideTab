17cd8e0f551ae57adc100d9d55dba017668fbb06
Maria Nilga <mnilga@gmail.com>
2015-06-23 10:16:22 +0300

    Initial commit

---
+++ app/src/main/java/com/nilga/masha/exampleslidetab2/MainActivity.java
@@ -0,0 +1,119 @@
+package com.nilga.masha.exampleslidetab2;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    // Declaring Your View and Variables
+
+    private Toolbar toolbar;
+    private ViewPager pager;
+    private ViewPagerAdapter adapter;
+    private SlidingTabLayout tabs;
+    private int Numboftabs =30;
+    private CharSequence Titles[]=new CharSequence[Numboftabs];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Creating The Toolbar on the top activity.
+        toolbar = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Новые заявки");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Creating the bottom menu.
+        Toolbar toolbarBottom = (Toolbar)findViewById(R.id.toolbar_bottom);
+        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.status:
+                        // TODO
+
+                        return true;
+                    case R.id.new_request:
+                        // TODO
+
+                        return true;
+                    case R.id.current_request:
+                        // TODO
+
+                        return true;
+                    case R.id.done_request:
+                        // TODO
+
+                        return true;
+                    case R.id.enter_new:
+                        // TODO
+
+                        return true;
+                }
+                return false;
+            }
+        } );
+        toolbarBottom.inflateMenu(R.menu.menu_bottom);
+
+
+
+        for (int i = 0; i <30; i++ ){
+            Titles[i] = "" + (i+1);
+        }
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.help) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
